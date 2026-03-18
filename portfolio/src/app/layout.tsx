b75ea07d2b290e731c78e33560f31c4e916fcc78
Peter Nwakuba <165977045+orahboni@users.noreply.github.com>
2026-03-18 10:42:50 +0000

    Added Chat widget

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -25,6 +25,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <script>
+        {`function initFreshdesk() {
+  window.fdWidget.init({
+    token: "01KKWD4QCJNWPFDMF8WVPXRF10",
+    host: "https://rosemaryokeleghel19.freshdesk.com",
+    widgetId: "01KKWMTFJ2Y8V1NY6R40NDX4KM"
+  });
+}
+
+function initialize(i,t){var e;i.getElementById(t)?initFreshdesk():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://rosemaryokeleghel19.freshdesk.com/webchat/js/widget.js",e.onload=initFreshdesk,i.head.appendChild(e))}function initiateCall(){initialize(document,"Freshdesk-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);`}
+      </script>
       </head>
       <body className={`${montserrat.variable} ${geistMono.variable}`}>
         {children}
